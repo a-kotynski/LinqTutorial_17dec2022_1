52e5a1f80298fdfa1eb6388f3e7bad524c048e82
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-12-17 07:29:53 +0100

    Add project files.

---
+++ LinqTutorial_17dec2022_1/Pet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTutorial_17dec2022_1;
+
+public enum PetType
+{
+    Dog,
+    Cat,
+    Fish
+}
+internal class Pet
+{
+    public Pet(int id, string name, PetType type, float number)
+    {
+        Id = id;
+        Name = name;
+        Type = type;
+        Number = number;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public PetType Type { get; set; }
+    public float Number { get; set; }
+}
---
+++ LinqTutorial_17dec2022_1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LinqTutorial_17dec2022_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var numbers = new[] { 5, 9, 2, 12, 6 };
+
+        var areAllLargerThanZero = numbers.All(number => number > 0);
+        Console.WriteLine($"{nameof(areAllLargerThanZero)}: { areAllLargerThanZero}");
+
+        var pets = new[]
+        {
+            new Pet(1, "Hannibal", PetType.Fish, 1.1f),
+            new Pet(2, "Anthony", PetType.Cat, 2f),
+            new Pet(3, "Ed", PetType.Cat, 0.7f),
+            new Pet(4, "Taiga", PetType.Dog, 35f),
+            new Pet(5, "Rex", PetType.Dog, 40f),
+            new Pet(6, "Lucky", PetType.Dog, 5f),
+            new Pet(7, "Storm", PetType.Cat, 0.9f),
+            new Pet(8, "Nyan", PetType.Cat, 2.2f)
+        };
+
+        var doAllHaveNonEmptyNames = pets.All(pet => !string.IsNullOrEmpty(pet.Name));
+        Console.WriteLine($"{nameof(doAllHaveNonEmptyNames)}: {doAllHaveNonEmptyNames}");
+
+        var areAllCats = pets.All(pet => pet.Type == PetType.Cat);
+        Console.WriteLine($"{nameof(areAllCats)}: {areAllCats}");
+
+    }
+}
